f3e5f78e2a82a379e247e4b084932d589dc24fe6
erikaleon <erikaleon2@gmail.com>
2023-09-15 14:51:54 +0200

    convert year to string

--- routes/year.js
+++ routes/year.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    const year = new Date().getFullYear();
+    const year = new Date().getFullYear().toString();
     res.json({ year: year });
    });
 
